c1dda9437daab719b1b0379c0e72aae353cc7146
Felipe López <felipeandreslopez91@gmail.com>
2019-08-22 22:47:12 -0500

    Se inicia el desarrollo de las páginas Home y Skills

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "normalize.css";
+import "./css/global.css";
+import "./css/flexboxgrid.css";
+
+const container = document.getElementById("app");
+
+ReactDOM.render(<App />, container);
